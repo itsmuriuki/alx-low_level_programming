1db8037c31e0e98ae9f7d2cbf26f886dd5fa0c02
itsmuriuki <grdmuriuki@gmail.com>
2021-02-03 19:52:08 +0000

    adding comments

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* using print f */
+/**
+*main - Entry point
+*Return: Always 0(success)
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
