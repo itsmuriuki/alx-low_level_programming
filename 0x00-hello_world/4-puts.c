c9ddacf356760f07ada21a84c6ba8c94a7bfc50e
itsmuriuki <grdmuriuki@gmail.com>
2021-02-03 15:51:54 +0000

    changes in 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/* using putsis less expensive and puts the cursor in the next line good with security */
+/* using puts is less expensive */
 int main(void)
 {
 puts(" \"Programming is like building a multilingual puzzle");
