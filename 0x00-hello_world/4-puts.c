9bde0a130b8e673668355bb67256de6332ce6bc3
itsmuriuki <grdmuriuki@gmail.com>
2021-02-03 15:49:03 +0000

    minor changes 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-/*
- *a program with a void
- *using putsis less expensive and puts the cursor in the next line good with security 
-*/
-
+/* using putsis less expensive and puts the cursor in the next line good with security */
 int main(void)
 {
 puts(" \"Programming is like building a multilingual puzzle");
- return (0);
+return (0);
 }
+
