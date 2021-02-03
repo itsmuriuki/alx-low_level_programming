4d5dbb87e75738ab39d2069336728f7d640d61b9
itsmuriuki <grdmuriuki@gmail.com>
2021-02-03 15:18:09 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/*
+ *a program with a void
+ *using putsis less expensive and puts the cursor in the next line it is also used in stead of printf for security reasons 
+
+*/
 int main(void)
 {
-  puts(" \"Programming is like building a multilingual puzzle") ;
+  puts(" \"Programming is like building a multilingual puzzle");
   return (0);
 }
 
