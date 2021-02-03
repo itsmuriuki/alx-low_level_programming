180ba2ad6d97e629851a84cf086179b039e0ea71
itsmuriuki <grdmuriuki@gmail.com>
2021-02-03 15:56:03 +0000

    changes in removing space

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /* using puts is less expensive */
 int main(void)
 {
-puts(" \"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
