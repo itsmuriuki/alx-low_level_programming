97d00d04ee1959d077685ecc17d154a706b6574c
itsmuriuki <grdmuriuki@gmail.com>
2021-02-05 09:02:16 +0000

    removing indentations

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,18 +10,17 @@
  */
 int main(void)
 {
-  int n;
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive", n);
+else if (n < 0)
+printf("%d is negative", n);
+else
+printf("%d is zero", n);
 
-  if (n > 0)
-    printf("%d is positive", n);
-  else if (n < 0)
-    printf("%d is negative", n);
-  else
-    printf("%d is zero", n);
-
-  putchar('\n');
-  return (0);
+putchar('\n');
+return (0);
 }
