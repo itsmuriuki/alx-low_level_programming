1511cb7c927096b30ac80296aac41cf66b244b35
itsmuriuki <grdmuriuki@gmail.com>
2021-02-09 18:40:04 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *print_alphabet - prints the letters from a to z in lowercase
+ *
+ *Return: 0 upon successful completion
+ */
+void print_alphabet(void)
+{
+  int letter = 'a';
+
+  while (letter <= 'z')
+    {
+      _putchar(letter);
+      letter++;
+    }
+  _putchar('\n');
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,8 +1,6 @@
-/*DHK header files*/
-#ifndef holberton
-#define holberton
-
-int _putchar(char c);
+unsigned long int _llpow(int base, int exponent);
+unsigned long int chop(unsigned long int number, int);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -15,8 +13,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-void lessthan100greaterthan9(int product);
-void greaterthan99(int product);
-void lessthan9(int product, int column);
-
-#endif
+void putformatnumber(int);
+void _putnumber(int);
