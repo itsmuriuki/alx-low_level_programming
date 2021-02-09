a7e029c7ca8c8fc9e71c3fcf943dab7cc6d9ed97
itsmuriuki <grdmuriuki@gmail.com>
2021-02-09 17:39:41 +0000

    printing char

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *main - Entry point function
+ *
+ *Description: This function prints "DHK"
+ *Return: 0 upon successful completion
+ */
+int main(void)
+{
+  _putchar('H');
+  _putchar('o');
+  _putchar('l');
+  _putchar('b');
+  _putchar('e');
+  _putchar('r');
+  _putchar('t');
+  _putchar('o');
+  _putchar('n');
+  _putchar('\n');
+  return (0);
+}
