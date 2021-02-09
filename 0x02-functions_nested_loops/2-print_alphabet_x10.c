9edf7f1f9b648cd21f5eace637bdc9b63d9d81a3
itsmuriuki <grdmuriuki@gmail.com>
2021-02-09 18:45:25 +0000

    printing alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet, ten times
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+  int a, n;
+
+  n = 0;
+
+  while (n < 10)
+    {
+      a = 'a';
+      while (a <= 'z')
+	{
+	  _putchar(a++);
+	}
+      _putchar(10);
+      n++;
+    }
+
+  return;
+
+}
