437f90638ae9b8c464b5f0e5f2f72cdcac04aa11
itsmuriuki <grdmuriuki@gmail.com>
2021-02-10 14:09:57 +0000

    2-largest bugs

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -14,4 +14,6 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
 #endif
